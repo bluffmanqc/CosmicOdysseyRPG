557e615404539e95a38663a300ec8912d10db3a0
bluffmanqc <Bluffmanqc@gmail.com>
2026-07-24 17:02:52 +0000

    update

--- app/src/main/java/com/cosmicodyssey/rpg/models/Character.java
+++ app/src/main/java/com/cosmicodyssey/rpg/models/Character.java
@@ -157,35 +157,9 @@ public class Character {
         return sb.toString();
     }
 
+    // Alias pour generateImagePrompt (compatibilité)
     public String generateImagePrompt() {
-        String armorName = "";
-        String weaponName = "";
-        
-        for (Equipment e : equipments) {
-            if ("armor".equals(e.getType())) {
-                armorName = e.getName();
-            } else if ("weapon".equals(e.getType())) {
-                weaponName = e.getName();
-            }
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        sb.append("A space warrior");
-        
-        if (!armorName.isEmpty()) {
-            sb.append(" wearing ").append(armorName);
-        }
-        
-        if (!weaponName.isEmpty()) {
-            sb.append(", holding ").append(weaponName);
-        }
-        
-        if (mount != null) {
-            sb.append(", riding ").append(mount.getName());
-        }
-        
-        sb.append(" in a sci-fi galaxy, dynamic pose, neon lights");
-        
-        return sb.toString();
+        return getCharacterPrompt();
     }
+
 }
